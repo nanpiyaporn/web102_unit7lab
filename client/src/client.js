3797b45d4dd304d2183321c7d93a0549bc56ee81
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-04-13 00:14:03 -0400

    Update .env file and remove client/src/.gitignore

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+
 const URL = 'https://zztckcckxbczjnjebicw.supabase.co';
 
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6dGNrY2NreGJjempuamViaWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5NjM4NjYsImV4cCI6MjAyODUzOTg2Nn0.32BeRJRh9KPLT2yjjnaRkZMTwwrixPle8B3Af8JJzBg';
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 
 
